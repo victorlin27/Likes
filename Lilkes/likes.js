cc7dc4421d4d40cfc17b8b3289aa49c2aa844611
victorlin27 <97265213+victorlin27@users.noreply.github.com>
2023-05-08 19:46:18 -0700

    Add files via upload

---
+++ Lilkes/likes.js
@@ -0,0 +1,10 @@
+var count =3;
+var countElement = document.querySelector("#count")
+
+console.log(countElement)
+
+function add1() {
+    count++;
+    countElement.innerText = count + " like(s)"
+    console.log(count);
+}
---
+++ Lilkes/likes2.js
@@ -0,0 +1,32 @@
+var count =9;
+var countElement = document.querySelector("#count")
+
+console.log(countElement)
+
+function add1() {
+    count++;
+    countElement.innerText = count + " like(s)"
+    console.log(count);
+}
+
+var counts =12;
+var countsElement = document.querySelector("#counts")
+
+console.log(countsElement)
+
+function add2() {
+    counts++
+    countsElement.innerText = counts + " like(s)"
+    console.log(counts)
+}
+
+var county =9;
+var countyElement = document.querySelector("#county")
+
+console.log(countyElement)
+
+function add3() {
+    county++;
+    countyElement.innerText = county + " like(s)"
+    console.log(county)
+}``
